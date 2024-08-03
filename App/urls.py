68d1facd10ebc847259014497117bc547e552882
Vaishnavi Bhoyar <vaishubhoyar02@gmail.com>
2024-08-03 16:32:21 +0530

    page

--- App/urls.py
+++ App/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 urlpatterns= [
     path('',home,name='home'),
    path('product/',product,name='product'),
-   path('about/',about,name='about')
+   path('about/',about,name='about'),
+   path('login/',login,name='login')
+
 
 ]
--- App/views.py
+++ App/views.py
@@ -9,3 +9,6 @@ def product(request):
 
 def about(request):
     return render(request,'about.html')
+
+def login(request):
+    return render(request,'login.html')
