69c0f3c98842cc63058dd8c73ccca0563eea5bf8
Vaishnavi Bhoyar <vaishubhoyar02@gmail.com>
2024-08-28 22:57:08 +0530

    page

--- Project/urls.py
+++ Project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app', include('App.urls')),
-    path('', include('app2.urls'))
+    path('', include('App.urls')),
+    path('app', include('app2.urls'))
 ]
